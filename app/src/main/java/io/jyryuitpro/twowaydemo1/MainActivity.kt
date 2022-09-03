8e8f85c02e4d5e83012b0ddeed207861fbc0c408
jyr <jyr@wavers.co.kr>
2022-09-03 18:23:59 +0900

    Two Way Data Binding

--- app/src/main/java/io/jyryuitpro/twowaydemo1/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/twowaydemo1/MainActivity.kt
@@ -2,10 +2,21 @@ package io.jyryuitpro.twowaydemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import io.jyryuitpro.twowaydemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
---
+++ app/src/main/java/io/jyryuitpro/twowaydemo1/MainActivityViewModel.kt
@@ -0,0 +1,13 @@
+package io.jyryuitpro.twowaydemo1
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+    val userName = MutableLiveData<String>()
+
+    init {
+        userName.value = "류지영"
+    }
+}
